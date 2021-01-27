007ee564d9cd02275aa0658d124d80fa0757323a
GossipCoco <gossipcoco@hotmail.fr>
2021-01-27 16:22:39 +0100

    emit data form

--- src/components/Users/UserDetails.vue
+++ src/components/Users/UserDetails.vue
@@ -2,12 +2,12 @@
 <template>
     <div class="user-personnal-info">
         <p>Information sur l'utilisateur</p>
-        <p>Nom : {{ usrInfo.name }}</p>
+        <p>Nom : {{ usrName }}</p>
         <p>Nom d'usage : {{ usrInfo.username }}</p>
         <p>Email : {{ usrInfo.email }}</p>
         <button v-on:click="showFormEdit()">Modifier des informations</button>
         <div v-if="showFormEditContaine === true">
-            <user-form-edit v-bind:userPersonnalInfo="usrInfo"></user-form-edit>
+            <user-form-edit v-bind:userPersonnalInfo="usrInfo" @editInfoUsrSuccess="editInfoUsrSuccess"></user-form-edit>
         </div>
     </div>
 </template>
@@ -22,11 +22,16 @@ export default {
     data (){
         return{
             showFormEditContaine: false,
+            usrName: this.usrInfo.name
         }
     },
     methods:{
         showFormEdit(){
             this.showFormEditContaine = true
+        },
+        editInfoUsrSuccess(value){
+            this.usrName = value
+            console.log(value)
         }
     }
 }
--- src/components/Users/UserFormEdit.vue
+++ src/components/Users/UserFormEdit.vue
@@ -1,13 +1,50 @@
 <template>
     <div>
         <p>
-            Modifier un user
+            Modifier {{usrName}}
         </p>
+        <div class="form-edit">
+            <form id="editForm">
+                <label>Nom</label>
+                <input
+                type="text"
+                v-model="usrName"> <br>
+                <label>Email</label>
+                <input type="email"
+                v-model="usrEmail"> <br>
+                <button v-on:click="editUserInformation">Modifier</button>
+            </form>
+            <p>{{answerName}}</p>
+            <p>{{answerEmail}}</p>
+        </div>
     </div>
 </template>
 <script>
 export default {
     name:'UserFormEdit',
-    props: ['usrInfo'],
+    props: ['userPersonnalInfo'],
+    data () {
+        return {
+            usrEditInfo: [],
+            usrInfo: this.userPersonnalInfo,
+            usrName: this.userPersonnalInfo.name,
+            usrEmail: this.userPersonnalInfo.email,
+            answerName: '',
+            answerEmail: '',
+        }
+    },
+    watch: {
+        usrName: function (newName, oldName) {            
+            this.answerName = "Ancien nom : " + oldName + " - Nouveau nom : " + newName            
+        },
+        usrEmail: function(newEmail, oldEmail){
+            this.answerEmail = "Ancien email : " + oldEmail + " - Nouvel Email : " + newEmail
+            }
+    },
+    methods: {
+        editUserInformation (value) {
+            this.$emit('editInfoUsrSuccess', this.usrName);
+        },
+    }
 }
 </script>
